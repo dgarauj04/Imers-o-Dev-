204a54a01f48440f03bed25f8a5e6c22119e9e97
Douglas Araujo <douglasdab10@gmail.com>
2024-09-07 21:49:27 -0300

    Add files via upload
    FindMyAi é uma plataforma inovadora dedicada a ajudar usuários a encontrar as melhores ferramentas de inteligência artificial (IA) de acordo com suas necessidades específicas. Com uma interface amigável e um sistema de pesquisa avançado, FindMyAi permite que você descubra e explore uma vasta gama de ferramentas de IA, desde assistentes virtuais até soluções de análise de dados, tudo de forma rápida e eficiente. Seja você um profissional buscando otimizar seu trabalho ou um entusiasta de tecnologia.

---
+++ script.js
@@ -0,0 +1,280 @@
+// dados.js
+let dados = [
+  {
+  titulo : "Google Gemini",
+  descricao : "O Google Gemini representa um salto significativo na área da inteligência artificial, oferecendo uma gama de possibilidades para o futuro. Sua capacidade de aprender, criar e se adaptar o torna uma ferramenta poderosa com um grande potencial para transformar diversas áreas da nossa vida.",
+  link : "https://gemini.google.com/?hl=pt-BR",
+   tags : "assistente virtual, criar, NLP, IA, Google, aprendizado, adaptação, criação"
+},
+
+ {
+  titulo : "Grammarly",
+  descricao : "Corretor gramatical e de estilo com IA que ajuda você a escrever de forma mais clara e profissional. Usada para revisar e corrigir textos em tempo real, oferecendo sugestões para melhorar a clareza e a eficácia da escrita. Grammarly é amplamente utilizada por estudantes, profissionais e escritores para garantir que seus textos sejam claros, corretos e bem estruturados. A ferramenta também oferece funcionalidades avançadas, como verificação de plágio e sugestões de tom de voz.",
+  link : "https://www.grammarly.com",
+  tags : "correção gramatical, escrita assistida, sugestões, verificação de plágio, edição de texto"
+},
+  
+ {
+  titulo : "Blackbox.ia",
+  descricao : "Ferramenta de geração de código impulsionada por IA que ajuda desenvolvedores a escrever código mais rápido e melhor. Sugestões de código, geração de snippets, otimização de erros. Suporta mais de 20 linguagens de programação e integra-se com VSCode.",
+  link : "https://www.blackbox.ai/",
+  tags : "automação, análise de dados, inteligência artificial, machine learning, otimização, códigos, programação"
+},
+
+{
+  titulo : "Hugging Face Transformers",
+  descricao : "Biblioteca que oferece APIs e ferramentas para baixar e treinar modelos de IA de última geração. Processamento de linguagem natural, visão computacional, reconhecimento de fala. Suporta PyTorch, TensorFlow e JAX.",
+  link : "https://huggingface.co/docs/transformers/index",
+  tags : "processamento de linguagem natural, NLP, modelos de linguagem, transformers, IA"
+},
+
+{
+  titulo : "GitHub Copilot",
+  descricao : "Assistente de programação em pares que ajuda a escrever código mais rápido e com menos esforço. Sugestões de código em tempo real, explicações de código, geração de testes. Funciona bem com várias linguagens e frameworks populares",
+  link : "https://github.com/features/copilot",
+  tags : "assistente de programação, codificação, sugestão, desenvolvimento de software, explicação"
+},
+
+{
+  titulo : "Google Bard",
+  descricao : "Chatbot de IA desenvolvido pelo Google para ajudar os usuários em várias tarefas. Organização de informações, resumo de artigos, criação de roteiros de viagem. Baseado no modelo de linguagem PaLM 2",
+  link : "https://gemini.google.com/?hl=pt-BR",
+  tags : "criação de texto, criatividade, escrita assistida, organizar, resumos"
+},
+
+{
+  titulo : "Tabnine",
+  descricao : "Assistente de código que acelera o desenvolvimento de software enquanto mantém a privacidade do código.  Geração de código, automação de tarefas repetitivas, sugestões baseadas em contexto. Suporta várias linguagens e IDEs populares.",
+  link : "https://www.tabnine.com/",
+  tags : "completação de código, assistente de programação, IA, desenvolvimento de software"
+},
+
+{
+  titulo : "iZotope RX",
+  descricao : "Ferramenta de edição de áudio que utiliza IA para restaurar e melhorar a qualidade do som. Remoção de ruídos, restauração de áudio, mixagem. Amplamente utilizada em produção musical e pós-produção de áudio.",
+  link : "https://www.izotope.com/en/products/rx.html",
+  tags : "edição de áudio, restauração de áudio, produção musical, mixagem, remoção de ruídos"
+},
+
+{
+  titulo : "Jukebox",
+  descricao : "Jukebox é um modelo de IA desenvolvido pela OpenAI que gera música com vocais em vários estilos e gêneros, permitindo a criação de novas composições musicais.",
+  link : "https://openai.com/research/jukebox",
+ tags : "geração de música, gêneros, modelos de linguagem, OpenAI"
+},
+
+{
+  titulo : "Soundraw",
+  descricao : "Soundraw é uma ferramenta de geração de música baseada em IA que permite aos usuários criar músicas personalizadas para vídeos, jogos e outros projetos criativos.",
+  link : "https://soundraw.io/",
+  tags : "criação de música, personalização, produção musical, criatividade"
+},
+
+{
+  titulo : "LumaFusion",
+  descricao : "Aplicativo de edição de vídeo que utiliza IA para facilitar a criação de vídeos profissionais. Edição de vídeo, adição de efeitos, correção de cores. Popular entre criadores de conteúdo e profissionais de vídeo.",
+  link : "https://luma-touch.com/lumafusion",
+  tags : "edição de vídeo, produção de vídeo, multimídia, correção de cores"
+},
+
+{
+  titulo : "RunwayML",
+  descricao : "Plataforma que oferece ferramentas de IA para criação de conteúdo multimídia. Facilita a edição de vídeo, geração de imagens, efeitos visuais. Integra-se com várias ferramentas de criação de conteúdo.",
+  link : "https://runwayml.com/",
+  tags : "criação de conteúdo, edição de vídeo, modelos de IA, multimídia, efeitos visuais"
+},
+
+{
+  titulo : "Autopan",
+  descricao : "Autopan é uma ferramenta de áudio que automatiza o processo de pan (distribuição do som entre os canais esquerdo e direito), permitindo a criação de efeitos sonoros dinâmicos.",
+  link : "https://www.soundtoys.com/product/panman/",
+  tags : "edição de áudio, efeitos de áudio, produção musical, sonoros"
+},
+
+{
+  titulo : "Adobe Sensei",
+  descricao : "Plataforma de IA da Adobe que melhora a criação de conteúdo digital. Edição de imagens, design gráfico, automação de tarefas criativas. Integrada com a Creative Cloud da Adobe.",
+  link : "https://www.adobe.com/br/sensei.html",
+  tags : "automação, edição de imagem, edição de vídeo, design gráfico"
+},
+
+{
+  titulo : "Synthesia",
+  descricao : "Synthesia é uma plataforma de criação de vídeos que utiliza IA para gerar vídeos com avatares realistas, permitindo a criação de conteúdo de vídeo de forma rápida e eficiente.",
+  link : "https://www.synthesia.io/",
+  tags : "criação de vídeo, avatares virtuais, realistas, multimídia"
+},
+
+{
+  titulo : "DALL-E 2",
+  descricao : "DALL-E 2 é um modelo de IA desenvolvido pela OpenAI que gera imagens a partir de descrições textuais, permitindo a criação de arte digital e ilustrações de alta qualidade.",
+  link : "https://openai.com/dall-e-2",
+  tags : "geração de imagens, criação, criatividade, OpenAi, arte digital"
+},
+
+{
+  titulo : "Midjourney",
+  descricao : "Midjourney é uma ferramenta de IA que gera imagens artísticas e criativas a partir de descrições textuais, permitindo a criação de obras de arte únicas.",
+  link : "https://www.midjourney.com/",
+  tags : "geração de imagens, arte digital, artes, criatividade"
+},
+
+{
+  titulo : "Copy.ai",
+  descricao : "Copy.ai é uma ferramenta de escrita assistida por IA que ajuda a criar conteúdo de marketing, posts em redes sociais, e-mails e muito mais, de forma rápida e eficiente.",
+  link : "https://www.copy.ai/",
+  tags : "geração de texto, marketing de conteúdo, escrita assistida, posts"
+},
+
+{
+  titulo : "Jasper.ai",
+  descricao : "Ferramenta de escrita assistida por IA para marketing e criação de conteúdo. Geração de blogs, e-mails, scripts de vídeo. Oferece templates personalizáveis para diferentes tipos de conteúdo.",
+  link : "https://www.jasper.ai/",
+  tags : "geração de texto, marketing de conteúdo, escrita assistida, blogs, scripts  vídeo, e-mails"
+},
+
+{
+  titulo : "ChatGPT",
+  descricao : "ChatGPT é um modelo de linguagem desenvolvido pela OpenAI capaz de gerar textos, traduzir idiomas, escrever diferentes tipos de conteúdo criativo, responder a suas perguntas de forma informativa e articipando de diálogos de maneira natural.",
+  link : "https://chat.openai.com/",
+  tags : "assistente virtual, conversação, NLP, OpenAI, criação, tradução, escrita"
+},
+
+{
+  titulo : "HubSpot",
+  descricao : "HubSpot oferece ferramentas para criação de campanhas de marketing personalizadas, atendimento ao cliente com recursos de IA e análise de desempenho.Hubspot é uma plataforma de CRM que utiliza inteligência artificial para automação de marketing e análise de dados",
+  link : "https://www.hubspot.com/",
+  tags : "CRM, marketing, automação, atendimento, análise"
+},
+
+{
+  titulo : "Zendesk",
+  descricao : "Plataforma de atendimento ao cliente que utiliza IA para melhorar a experiência do usuário.  Usada para gerenciar tickets de suporte e interações com clientes. Zendesk oferece automação de respostas e análise de sentimentos para melhorar o atendimento.",
+  link : "https://www.zendesk.com/",
+  tags : "atendimento ao cliente, suporte técnico, automação, interação com clientes"
+},
+
+{
+  titulo : "Power BI",
+  descricao : "Ferramenta da Microsoft para visualização de dados e business intelligence. Permite a criação de relatórios e dashboards interativos. Integrado com outros produtos Microsoft, facilitando a análise de dados provenientes de diversas fontes.",
+  link : "https://powerbi.microsoft.com/",
+  tags : "assistente de produtividade, automação, IA, Microsoft, dados, dashboards"
+},
+
+{
+  titulo : "Freshdesk",
+  descricao : "Software de helpdesk que utiliza IA para otimizar o suporte ao cliente. Facilita a gestão de tickets, oferece soluções para vendas e facilita a comunicação com clientes. Freshdesk inclui chatbots e automação de tarefas repetitivas ajudando as empresas a gerenciar e suporta de forma eficiente.",
+  link : "https://freshdesk.com/",
+  tags : "atendimento ao cliente, suporte técnico, automação, helpdesk, soluções para vendas"
+},
+
+{
+  titulo : "Tableau",
+  descricao : "Ferramenta de visualização de dados que ajuda a transformar dados em insights acionáveis. Usada para criar gráficos interativos e dashboards. Tableau é amplamente utilizado em empresas para análise de dados e tomada de decisões baseadas em dados.",
+  link : "https://www.tableau.com/",
+  tags : "visualização de dados, análise de dados, BI, IA, gráficos, dashboard"
+},
+
+{
+  titulo : "Marketo",
+  descricao : "Plataforma de automação de marketing que utiliza IA para otimizar campanhas. Ajuda a gerenciar campanhas de marketing digital e a analisar resultados. Marketo é conhecida por suas capacidades avançadas de segmentação e personalização de campanhas.",
+  link : "https://www.marketo.com/",
+  tags : "automação de marketing, CRM, análise de dados, IA"
+},
+
+
+{
+  titulo : "Workday",
+  descricao : "Sistema de gestão de capital humano que utiliza IA para otimizar processos de RH. Usado para gestão de talentos, folha de pagamento e análise de desempenho. Workday oferece insights baseados em dados para melhorar a tomada de decisões em RH.",
+  link : "https://www.workday.com/",
+  tags : "gestão de recursos humanos, ERP, automação, IA, RH"
+},
+
+
+{
+  titulo : "BambooHR",
+  descricao : "Software de RH que utiliza IA para simplificar a gestão de funcionários. Facilita a gestão de recrutamento, onboarding e desempenho. BambooHR é conhecido por sua interface amigável e funcionalidades de automação de processos de RH.",
+  link : "https://www.bamboohr.com/",
+  tags : "gestão de recursos humanos, RH, automação, IA, onboarding, gestão"
+},
+  
+ { 
+  titulo : "Microsoft Copilot",
+  descricao : "Assistente de IA integrado ao Microsoft 365 para ajudar na criação de documentos. Sugestões de texto, automação de tarefas, análise de dados. Integrado com Word, Excel, PowerPoint e outras ferramentas do Microsoft 365.",
+  link : "https://www.microsoft.com/en-us/microsoft-365/copilot",
+  tags : "assistente de produtividade, automação, IA, Microsoft"
+}
+];
+
+function pesquisar() {
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+  
+  // se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Não foi possivel encontrar resultados. Você não digitou na barra de pesquisa</p>"
+    return
+  }
+  
+  campoPesquisa = campoPesquisa.toLowerCase()
+  
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+  
+  // Itera sobre os dados e constrói o HTML para cada resultado
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    //se titulo includes campoPequisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+       // Cria uma nova div para cada resultado
+    resultados += `
+      <div class="item-resultado">
+        <h2>
+          <a href=${dado.link} target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+       <a> <a href=${dado.link} target="_blank"> Clique aqui</a> para acessar a ferramenta</a>
+      </div>
+    `;
+    } 
+  }
+  
+  if (!resultados){
+    resultados = "<p>Não foi possivel encontrar resultados</p>"
+  }
+  
+  // Atribui o HTML gerado à seção de resultados
+  section.innerHTML = resultados;
+}
+
+function tudo() {
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+
+  // Itera sobre os dados e constrói o HTML para cada resultado
+  for (let dado of dados) {
+    // Cria uma nova div para cada resultado
+    resultados += `
+      <div class="item-resultado">
+        <h2>
+          <a href=${dado.link} target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank"> Mais sobre</a>
+      </div>
+    `;
+  }
+
+  // Atribui o HTML gerado à seção de resultados
+  section.innerHTML = resultados;
+}
